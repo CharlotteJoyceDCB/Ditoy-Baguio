1caa1b73f95fc1af3f9c243c44a3ea04c4f882c9
CharlotteJoyce <charlottejdcb@gmail.com>
2025-01-11 15:30:20 +0800

    Basic setup and added hero section.

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../HeroSection.scss";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <nav className="navbar">
+        <div className="logo">DITOY BAGUIO</div>
+        <ul className="nav-links">
+          <li><a href="#destination">WHERE TO GO</a></li>
+          <li><a href="#blogs">EVENTS</a></li>
+          <li><a href="#booking">BLOG</a></li>
+          <li><a href="#contact">ABOUT </a></li>
+        </ul>
+      </nav>
+      <div className="hero-content">
+        <p className="hero-text">
+        Discover the beauty and culture of the Summer Capital of the Philippines.
+        </p>
+        <button className="hero-button">
+          Take a tour <span>→</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
